79d3bcf2075bde9add13649bf0984d634f386249
davidpxn <xuanphuong97@gmail.com>
2019-07-06 22:04:40 +0000

    Footer component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Helmet from 'react-helmet';
+import { Route, Switch } from 'react-router-dom'
+
+import About from './routes/about/About';
+
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer'
+
+import './App.scss';
+import data from './data.json';
+
 
 function App() {
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Helmet defaultTitle="Portfolio" titleTemplate="Portfolio - %s" />
+      <Navbar tabs={data.categories} />
+      <Switch>
+        <Route exact path="/" component={About}/>
+      </Switch>
+      <Footer contactInfo={data.contactInfo}/>
     </div>
   );
 }
